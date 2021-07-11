865bb0caca1499f51ceb307550d6ae8cdf73d810
gumbbbb <87250855+gumbbbb@users.noreply.github.com>
2021-07-11 08:22:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Bạn uii!',
+    introDesc: `Trái đất có 7 tỉ ngừi
+    Đố bạn biết ai đẹp chai nhấc`,
+    btnIntro: 'hihi',
+    title: 'Đẹp chai nhấc thế giớiii',
+    desc: 'Đố biếc luông :3333 ',
+    btnYes: 'Tứn Anh chứ ai :333',
+    btnNo: 'Hông phải TA :0',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại thấy tui đẹp zai <3',
+    btnReply: 'Gửi cho TNTA <3',
+    reply: 'Tại dì Tứn Anh đáng iu chứ shao nựa <33333333',
+    mess: 'Tui biết mà 🥰. Yêu nhiều nhiều 😘😘',
+    messDesc: 'Nói chiện dới ngừi kuteee nhấc thế giới nào :3.',
+    btnAccept: 'Okiiiii ',
+    messLink: 'https://m.me/as.cacf' //m.melink mess của các bạn. VD: https://m.me/nam.nodemy
+}
